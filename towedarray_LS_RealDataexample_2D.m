ac43223ff8dd1cd2e7bd248900afb5feec0a38cd
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-12-21 16:57:33 -1000

    Incorporated Dilation of Surfaces

--- towedarray_LS_RealDataexample_2D.m
+++ towedarray_LS_RealDataexample_2D.m
@@ -11,7 +11,7 @@
 % Also make sure you have extracted data from Pamguard sql database with
 % "Extract_Pamguard_detections" package
 
-%clear,close all
+clear,close all
 
 % /////////////////// ADD PATHS and EXTRACTED TRACKS: ////////////////////
 % Add path for plotting- change to reflect your path to the TDOA tracking package:
@@ -46,13 +46,14 @@ sig =0.003; %Determine sigma (standard deviation) for the Gaussian
 %sig=0.0024; % From Yvonnes paper
 sig_hyperbolas = 0.00003; % STD for plotting intersecting hyperbolas
 %(for visual assesment of how they are crossing)- needs to be small
+mean_horiz_swimspeed= 0.5; %mean horizontal swim speed (for sperm whale) [m/s] 
 
 %--------  Parameters for the boat and array  ---------------- 
 % Choose either: 
 % hyph_pos = 1 to simulate boat movement and hydrophone positions
 % hyph_pos = 2 to use the real data
 
-get_hyph_pos = 1; 
+get_hyph_pos = 2; 
 switch get_hyph_pos
     case 1
         d=parameters.d; %distance between sensors
@@ -65,6 +66,7 @@ switch get_hyph_pos
         
     case 2
        GPSandPosition_table=readtable('GPSandPosition_table.csv'); 
+       timestep = parameters.dt; % how much time elapses in each time step in s
 end
 %--------  Parameters for modeled TDOA ---------------- 
 % - Grid range and resolution:
@@ -144,7 +146,7 @@ end
 
 
 %% ////////// Get Hydrophone positions ///////////
-clear hyph_pos
+
 switch get_hyph_pos
     case 1
         % Simulate Boat movement and Hydrophone positions
@@ -198,9 +200,17 @@ tdoa_measured_select=sum(tdoa_measured(SelectedTracks,:),1,'omitnan');
 tdoa_measured_select(all(isnan(tdoa_measured(SelectedTracks,:)),1)) = NaN;
 
 %% ------------- Compute the Surface --------------
+%~~~~~~~~~ Get time step when animal is closest to 90deg (tdoa=0s)~~~~~~~~~
+% This is for the purpose of Surface Dilation
+[~,ind]=min(abs(tdoa_measured_select));
+tsteps=1:1:Ntsteps;
+tstep90=tsteps(ind); 
+
 % Pre-allocate
 LS_select= nan(1,N,Ntsteps);
 LS_Hyperbolas = LS_select;
+LSdilate = nan(Ngp_y,Ngp_x,Ntsteps); %swap x and y inpuput arguments since 
+% reshape() will be used to fill in the values
 
 count=1; plotf=0;
 for t=1:Ntsteps % for each time step compute LS
@@ -220,6 +230,29 @@ for t=1:Ntsteps % for each time step compute LS
         LS_select(:,:,t)= exp(-1/(2*sig^2).*tdoa_diff);
         LS_Hyperbolas(:,:,t)= exp(-1/(2*sig_hyperbolas^2).*tdoa_diff);
 
+        %///////////////////DILATE SURFACES////////////////////////////////
+        % Apply Surface dilation filter (imdilate) to compensate for whale movement:
+        LStotal_temp=reshape(LS_select(:,:,t),[Ngp_y,Ngp_x]);
+        dt90 = timestep*abs(t-tstep90); %Elapsed time from when animals are at 90deg (0 tdoa) [s]
+        if dt90==0
+
+            LSdilate(:,:,t)=LStotal_temp; %do not dilate if animal is at the beam (the tdoas should be correct)
+
+        else
+            mdwh=dt90*mean_horiz_swimspeed; %maximum distance whale could have travelled horizontally [m]
+
+            %gridpoints for filter in x (and also y since the same assumptions re swim speed and grid space)
+            xgridf=0:dx:(mdwh+dx);
+            filt_x_grid = [-fliplr(xgridf(2:end)),xgridf];
+            ygridf=0:dy:(mdwh+dy);
+            filt_y_grid = [-fliplr(ygridf(2:end)),ygridf];
+            [Fx,Fy] = meshgrid(filt_x_grid,filt_y_grid);
+            De = sqrt(Fx.^2+Fy.^2)/mdwh; %normalize to max distance that the whale could have swam (feasible distance will be 1 or lower)
+
+            LSdilate(:,:,t)=imdilate(LStotal_temp,(De<=1));
+        end
+        %//////////////////////////////////////////////////////////////////
+
         if any(count==plotf)
             figure,hold on
             LStotal_temp=reshape(LS_select(:,:,t),[Ngp_y,Ngp_x]);
@@ -235,6 +268,7 @@ for t=1:Ntsteps % for each time step compute LS
         end
 
         count=count+1;
+
     end
 end
 
@@ -262,6 +296,29 @@ legend('Ambiguity Surface',['Sensor ', num2str(hph),' position'],'Boat track')
 xlim([xrange(1),xrange(2)])
 ylim([yrange(1),yrange(2)])
 set(gca,'FontSize',16)
+
+%~~~~~~~~~~~~~~~~PLOT final Ambiguity Surface with Dilation~~~~~~~~~~~~~
+LSdilatetotal=prod(LSdilate,3,'omitnan');
+h=figure; hold on,
+s=pcolor(X,Y,LSdilatetotal);
+s.EdgeColor='none';
+clim([0,1])
+colorbar
+axis equal
+hph=1;
+plot(squeeze(hyph_pos(hph,1,:)),squeeze(hyph_pos(hph,2,:)),'r-','Linewidth', 3)
+switch get_hyph_pos
+    case 1
+        plot(x_boat, y_boat,'g-','Linewidth', 1.5)
+    case 2
+        plot(GPSandPosition_table.Boat_pos_x_m, GPSandPosition_table.Boat_pos_y_m,'g-','Linewidth', 1.5)
+end
+xlabel(' x (m)'),ylabel('y (m)')
+title(['Total ambiguity surface WITH dilation for source ', num2str(SelectedTracks)])
+legend('Ambiguity Surface',['Sensor ', num2str(hph),' position'],'Boat track')
+xlim([xrange(1),xrange(2)])
+ylim([yrange(1),yrange(2)])
+fontsize(h,16,'points')
 
 %~~~~~~~~~~~~~~~~~~~PLOT Intersecting Hyperbolas~~~~~~~~~~~~~~~~~~~
 LStotal_hyperbolas_temp = sum(LS_Hyperbolas,3,'omitnan');
