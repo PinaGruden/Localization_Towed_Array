e7b3e41e6fc081f32b0e47636838e010d9476c2e
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-12-28 10:36:12 -1000

    Created function to allow user to select tracks

---
+++ select_tracks.m
@@ -0,0 +1,41 @@
+function [tdoa_select, selected_indx] = select_tracks(Tracks,timevec,maxtdoa)
+% select_tracks.m allows user to select which TDOA track or which fragments
+% they want to use to obtain the localization for. 
+
+% INPUTS:
+% - Tracks: a structure containing tdoa tracks (with fields "time", "tdoa")
+% - timevec : a vector of times 
+% - maxtdoa : a scalar- maximum tdoa allowed
+
+% OUTPUTS:
+
+
+
+
+
+%---------- Re-arrange Tracks in a matrix (Nsources x Ntsteps)--------
+Ntsteps=numel(timevec); %number of time steps
+Nsources=size(Tracks,2);
+tdoa_measured= nan(Nsources,Ntsteps);
+for k=1:Nsources
+    time_indx=ismember(timevec,Tracks(k).time);
+    tdoa_measured(k,time_indx)= Tracks(k).tdoa;
+end
+
+%-------- Select which tracks you want to compute the surfaces for------
+prompt = "Which track/tracks you want to compute the ambiguity surface for? \n" + ...
+    " Note, enter a single track number or if track is fragmented, \n " + ...
+    "then enter fragments as a 1xN vector. \n " + ...
+    "Look at Figures 1 and 2 for track numbers. \n ";
+
+SelectedTracks =input(prompt);
+selected_indx=zeros(length(SelectedTracks),length(timevec));
+for n=1:length(SelectedTracks)
+ind=find(abs(Tracks(SelectedTracks(n)).tdoa)<maxtdoa);
+selected_indx(n,:)=ismember(timevec,Tracks(SelectedTracks(n)).time(ind));
+end
+selected_indx=sum(selected_indx,1);
+tdoa_select=sum(tdoa_measured(SelectedTracks,:),1,'omitnan');
+tdoa_select(all(isnan(tdoa_measured(SelectedTracks,:)),1)) = NaN;
+
+end
