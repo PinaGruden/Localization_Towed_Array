858e9fb84136cdcd0027ef6d92a34120030a569a
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-12-28 16:55:29 -1000

    added argument to plot estimated position

--- plot_AS.m
+++ plot_AS.m
@@ -1,4 +1,4 @@
-function plot_AS(AStotal,AS_params,hyph_pos,boat_pos)
+function plot_AS(AStotal,AS_params,hyph_pos,boat_pos,est_loc_m)
 %plotAS.m is a function that plots the final ambiguity surface (AStotal),
 %boat and array position.
 
@@ -11,6 +11,8 @@ function plot_AS(AStotal,AS_params,hyph_pos,boat_pos)
 %              sensors and M is number of coordinates (e.g. x,y or x,y,z)
 % - boat_pos : a N x 2 matrix, where 1st column contains x-coordinates
 % (in m) and 2nd column contains y-coordinates (in m) of boat position
+% - est_loc_m : 1x2 vector indicating x and y coordinates of the estimated
+%               source location (in m)
 
 % Pina Gruden, Dec 2022, UH Manoa
 
@@ -28,9 +30,10 @@ axis equal
 hph=1;
 plot(squeeze(hyph_pos(hph,1,:)),squeeze(hyph_pos(hph,2,:)),'r-','Linewidth', 3)
 plot(boat_pos(:,1), boat_pos(:,2),'g-','Linewidth', 1.5)
-    
+plot(est_loc_m(1),est_loc_m(2),'r*', 'MarkerSize',10,'Linewidth', 2) 
+
 xlabel(' x (m)'),ylabel('y (m)')
-legend('Ambiguity Surface',['Sensor ', num2str(hph),' position'],'Boat track')
+legend({'Ambiguity Surface',['Sensor ', num2str(hph),' position'],'Boat track', 'Estimated source location'})
 xlim([xrange(1),xrange(2)])
 ylim([yrange(1),yrange(2)])
 set(gca,'FontSize',16)
