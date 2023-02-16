8db9ba51214647ab89f28ec3947c76e92135d254
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2023-02-15 16:53:48 -1000

    added extraction of 2 localization peaks per AS

--- AS_Localization_code/localize_track.m
+++ AS_Localization_code/localize_track.m
@@ -67,57 +67,43 @@ function [SelectedTracks,AStotal,ASdilatetotal,AStotal_hyperbolas,Loc_table] = l
 
 %//////////////////////////////////////////////////////////////////////////
 %% //////////////////// 2) Compute Ambiguity Surface ///////////////////
-
+tic
 [AStotal,ASdilatetotal,AStotal_hyperbolas]= computeAS(tdoa_measured_select, ...
     selected_indx,hyph_pos,AS_params,BA_params);
-
-
+toc
 %//////////////////////////////////////////////////////////////////////////
 %% //////////////////// 3) Extract Localization info ///////////////////
 
-% NEED TO CHANGE THIS SECTION- at the moment I'm just selecting one max
-% peak... In theory the peak on the other side should be at the same
-% location but it's not in reality..
-
-% % ONE WAY:
-% [peakdata] =findpeaks2D(AS_params.X,AS_params.Y,AStotal);
-% 
-% disp(' ')
-% disp(['Estimated location for source [', num2str(SelectedTracks),'] is:'])
-% disp(' ')
-% 
-% %For the moment take just one of the estimated peaks- CHANGE THIS
-% [~,n]=max(peakdata.peakZ);
-% estimated_position=[peakdata.peakX(n),peakdata.peakY(n)]; %were at the moment considering 2D, so z coordinate = 0.
-% fprintf(['Estimated source location is: [', repmat('%g, ', 1, numel(estimated_position)-1), '%g]\n'],estimated_position)
-% fprintf('Width of the peak in X dirextion %.2f \n',peakdata.peakXWidth(n))
-% fprintf('Width of the peak in Y dirextion %.2f \n',peakdata.peakYWidth(n))
-% fprintf('Ambiguity value for the estimated location is %.2f \n',peakdata.peakZ(n))
-% hold on, plot3(peakdata.peakX(n),peakdata.peakY(n),peakdata.peakZ(n), 'k*', 'MarkerSize',6)
-
-% ANOTHER WAY:
-ind_max_peak = find(AStotal == max(max(AStotal))); %corresponds to max of x y z. turns into column
+% Select two largest peaks:
+maxval=maxk(AStotal(:),2); %find two largest peaks in the ambiguity surface
+ind_max_peak = [find(AStotal==maxval(1));find(AStotal==maxval(2))]; %indices of 2 maximum peaks
 estim_location_m = [AS_params.X(ind_max_peak),AS_params.Y(ind_max_peak)];
 estim_location_latlong = M2LatLon(estim_location_m,[boat_start_latlong(1),boat_start_latlong(2)]);
 
-ind_max_peak = find(ASdilatetotal == max(max(ASdilatetotal))); %corresponds to max of x y z. turns into column
+maxval=maxk(ASdilatetotal(:),2); %find two largest peaks in the dilated ambiguity surface
+ind_max_peak = [find(ASdilatetotal==maxval(1));find(ASdilatetotal==maxval(2))]; %indices of 2 maximum peaks
 estim_location_m_dilated = [AS_params.X(ind_max_peak),AS_params.Y(ind_max_peak)];
 estim_location_latlong_dilated = M2LatLon(estim_location_m_dilated,[boat_start_latlong(1),boat_start_latlong(2)]);
 
 %//////////////////////////////////////////////////////////////////////////
 %% //////////////////// 4) Compute perpendicular distance ///////////////////
 
-%get coefficients for a line equation for a boat:
+%g et coefficients for a line equation for a boat:
 p=polyfit(boat_pos(:,1),boat_pos(:,2),1);
 % p =[p1,p0] - where p1= slope and p0= intercept (coeffs of p are in
 % descending powers)
 
-d1=abs(p(1)*estim_location_m(1) - estim_location_m(2) + p(2))./sqrt(p(1)^2+1^2);
-d2=abs(p(1)*estim_location_m_dilated(1) - estim_location_m_dilated(2) + p(2))./sqrt(p(1)^2+1^2);
+% compute perpendicular distance
+d1=abs(p(1)*estim_location_m(:,1) - estim_location_m(:,2) + p(2))./sqrt(p(1)^2+1^2);
+d2=abs(p(1)*estim_location_m_dilated(:,1) - estim_location_m_dilated(:,2) + p(2))./sqrt(p(1)^2+1^2);
+
+% get perpendicular line equation (slope and intercept):
+slope_perp =  -1/p(1); %slope of perpendicular line is equal to negative inverse of the slope of the line that it is perpendicular to.
+interc_perp = estim_location_m(:,2) - slope_perp.*estim_location_m(:,1); 
 
 
 %Create a table with localization info:
-Loc_table = table({SelectedTracks},estim_location_m,estim_location_latlong, ...
+Loc_table = table(repmat({SelectedTracks},2,1),estim_location_m,estim_location_latlong, ...
     estim_location_m_dilated,estim_location_latlong_dilated,d1,d2, ...
     'VariableNames',{'TrackID','Loc_m','Loc_LatLong','Loc_m_dilated', ...
     'Loc_LatLong_dilated','distance_m','distance_m_dilated'});
