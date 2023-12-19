b5e2a7c111bf8fbddd54ab373a99845c398d7fbc
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2023-12-18 14:13:23 -1000

    added a check for if folder exists and if not create it

--- specify_paths.m
+++ specify_paths.m
@@ -1,4 +1,6 @@
 function [folder, folder2save2] = specify_paths
+% specify_paths.m specifies paths for "Localization_Towed_Array" package
+%
 %CONFIGURE PATHS to :
 % 1) Results of Extracted TDOA tracks (from TDOA_tracking_masters package)
 % 2) Cross-correlogram info (from TDOA_tracking_masters package)
@@ -7,33 +9,70 @@ function [folder, folder2save2] = specify_paths
 % 4) Folder where all raw GPS and Array data is
 % 5) Folder where GPS and Sensor Position table should be saved
 % 6) Folder where localization results should be saved to
+%
+% OUTPUT:
+% - folder - a structure specifying paths to where data is
+% - folder2save2 - a structure specifying paths to where results are saved to.
+%
+%Pina Gruden, 2023, UH Manoa
+
 
 %1) Path to Results of Extracted TDOA tracks (TDOA_tracking_masters package):
-s=what('./Test_example/Data/TDOA_tracks/'); 
+myfolder = './Test_example/Data/TDOA_tracks/';
+if not(isfolder(myfolder)) % If the folder doesnt exist - throw error since you need data to localize.
+error(['The folder ', myfolder, ' does not exists. There is no data ' ...
+    'for localization! Run "TDOA_tracking_master" package first!'])
+end
+s=what(myfolder); 
 folder.tdoas = [s.path,'/'];
 
 %2) Path to Cross-correlogram information (TDOA_tracking_masters package):
 % If not available specify folder.crosscorr = [];
-s=what('./Test_example/Data/Crosscorrelograms/');
+myfolder = './Test_example/Data/Crosscorrelograms/';
+if not(isfolder(myfolder)) % If the folder doesnt exist
+    mkdir(myfolder) %make a folder    
+    disp(['WARNING: The specified folder ', myfolder, ' does not exists. Created a new folder.'])
+end
+s=what(myfolder); 
 folder.crosscorr = [s.path,'/'];
 
 %3) Path to Results of Extracted Pamguard detections (Extract_Pamguard_detections package):
 % If they are not available specify folder.pagmuard = [];
-s=what('./Test_example/Data/Pamguard_detections/'); 
+myfolder='./Test_example/Data/Pamguard_detections/'; 
+if not(isfolder(myfolder)) % If the folder doesnt exist
+    mkdir(myfolder) %make a folder    
+    disp(['WARNING: The specified folder ', myfolder, ' does not exists. Created a new folder.'])
+end
+s=what(myfolder); 
 folder.pamguard = [s.path,'/'];
 
 % 4) Path to all raw GPS and Array data
 % If GPS data not available specify folder.rawgps = [];
-s=what('./Test_example/Data/Raw_GPSandArray_info/'); 
+myfolder='./Test_example/Data/Raw_GPSandArray_info/'; 
+if not(isfolder(myfolder)) % If the folder doesnt exist
+    mkdir(myfolder) %make a folder  
+    disp(['WARNING: The specified folder ', myfolder, ' does not exists. Created a new folder.'])
+end
+s=what(myfolder); 
 folder.rawgps = [s.path,'/'];
 
 % 5) Path to GPS and Sensor Position table will be stored:
 % If GPS data not available specify folder.gps = [];
-s=what('./Test_example/Data/GPSandPos_info/'); 
+myfolder='./Test_example/Data/GPSandPos_info/'; 
+if not(isfolder(myfolder)) % If the folder doesnt exist
+    mkdir(myfolder) %make a folder  
+    disp(['WARNING: The specified folder ', myfolder, ' does not exists. Created a new folder.'])
+end
+s=what(myfolder); 
 folder.gps = [s.path,'/'];
 
 % 6) Path to where Localization Results will be stored:
-s=what('./Test_example/Results/'); 
+myfolder='./Test_example/Results/';
+if not(isfolder(myfolder)) % If the folder doesnt exist
+    mkdir(myfolder) %make a folder    
+    disp(['WARNING: The specified folder ', myfolder, ' does not exists. Created a new folder.'])
+end
+s=what(myfolder); 
 folder2save2 = [s.path,'/'];
 
 
