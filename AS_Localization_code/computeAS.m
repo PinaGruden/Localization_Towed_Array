7c8cc9e75a73557e7a6e4c80d7ae60b6fe119c24
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2024-02-15 16:01:50 -1000

    clean up AS computation

--- AS_Localization_code/computeAS.m
+++ AS_Localization_code/computeAS.m
@@ -26,19 +26,7 @@ function [AStotal,ASdilatetotal,AStotal_hyperbolas,gridparams] = computeAS(tdoa_
 % Pina Gruden, Dec 2022, UH Manoa
 
 
-% wpos=AS_params.wpos;
-% Ngp_x=AS_params.Ngp_x;
-% Ngp_y=AS_params.Ngp_y;
-% dx=AS_params.dx;
-% dy=AS_params.dy;
-% sig=AS_params.sig;
-sig_hyperbolas=AS_params.sig_hyperbolas;
-c= AS_params.c;
-mean_horiz_swimspeed = AS_params.mean_horiz_swimspeed;
-timestep = BA_params.timestep;
-
 Ntsteps = size(tdoa_measured_select,2); %number of time steps
-% N= size(wpos,1); %number of grid points to evaluate
 
 %~~~~~~~ 1) Get time step when animal is closest to 90deg (tdoa=0s)~~~~~~~~
 % This is for the purpose of Surface Dilation
@@ -56,63 +44,11 @@ xrange=AS_params.xrange;
 yrange=AS_params.yrange;
 [gridparams,wpos2D] = make_2Dgrids(xrange,yrange,dx,dy);
 
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2.b) Pre-allocate ~~~~~~~~~~~~~~~~~~~~~~~~~~
-AS_select_rough= nan(gridparams.Ngp_y,gridparams.Ngp_x,Ntsteps);%swap x and y inpuput arguments since 
-% reshape() will be used to fill in the values
-ASdilate_rough = AS_select_rough;
 
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2.b) Compute AS ~~~~~~~~~~~~~~~~~~~~~~~~~~
+[AStotal_rough,ASdilatetotal_rough] = obtain_ambiguitysurface(hyph_pos,selected_indx, ...
+    AS_params,BA_params,gridparams,wpos2D,tdoa_measured_select,sig,tstep90);
 
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2.c) Compute AS ~~~~~~~~~~~~~~~~~~~~~~~~~~
-%count=1; plotf=0;
-for t=1:Ntsteps % for each time step compute LS
-    if selected_indx(t)
-        rp=hyph_pos(:,:,t);
-        ip1=1;ip2=2;
-        % for each hypothetical grid position wpos(wpi) this calculates
-        % time between position and hydrophone position - dt1 (between
-        % hyph 1 and position wpos(wpi)), dt2 (between hyph2 and position wpos(wpi)), then
-        % tdoa_model is tdoa between the hyph1 and hyph2 if the source
-        % is at wpos(wpi).
-        dt1= 1/c.*sqrt((rp(ip1,1)-wpos2D(:,1)).^2 +(rp(ip1,2)-wpos2D(:,2)).^2);
-        dt2= 1/c.*sqrt((rp(ip2,1)-wpos2D(:,1)).^2 +(rp(ip2,2)-wpos2D(:,2)).^2);
-        tdoa_model=dt1-dt2;
-        tdoa_model=reshape(tdoa_model,[gridparams.Ngp_y,gridparams.Ngp_x]);
-
-        tdoa_diff=(tdoa_model-tdoa_measured_select(:,t)).^2;
-        AS_select_rough(:,:,t)= exp(-1/(2*sig^2).*tdoa_diff);
-     
-        %///////////////////DILATE SURFACES////////////////////////////////
-        % Apply Surface dilation filter (imdilate) to compensate for whale movement:
-        AStotal_temp=AS_select_rough(:,:,t);
-        dt90 = timestep*abs(t-tstep90); %Elapsed time from when animals are at 90deg (0 tdoa) [s]
-        if dt90==0
-
-            ASdilate_rough(:,:,t)=AStotal_temp; %do not dilate if animal is at the beam (the tdoas should be correct)
-
-        else
-            mdwh=dt90*mean_horiz_swimspeed; %maximum distance whale could have travelled horizontally [m]
-
-            %gridpoints for filter in x (and also y since the same assumptions re swim speed and grid space)
-            xgridf=0:dx:(mdwh+dx);
-            filt_x_grid = [-fliplr(xgridf(2:end)),xgridf];
-            ygridf=0:dy:(mdwh+dy);
-            filt_y_grid = [-fliplr(ygridf(2:end)),ygridf];
-            [Fx,Fy] = meshgrid(filt_x_grid,filt_y_grid);
-            De = sqrt(Fx.^2+Fy.^2)/mdwh; %normalize to max distance that the whale could have swam (feasible distance will be 1 or lower)
-
-            ASdilate_rough(:,:,t)=imdilate(AStotal_temp,(De<=1));
-        end
-        %//////////////////////////////////////////////////////////////////
-
-    end
-end
-clear AStotal_temp
-
-% Get total AS without dilation:
-AStotal_rough=prod(AS_select_rough,3,'omitnan');
-
-% Get total AS with dilation:
-ASdilatetotal_rough=prod(ASdilate_rough,3,'omitnan');
 
 %~~~~~~~2.d) Determine the peak (Loc) in AS to get narrower x/yrange~~~~~~~
 %Compute it for both normal AS and dilated and take whathever is smaller&bigger
@@ -220,67 +156,9 @@ xrange=xrange_new;
 yrange=yrange_new;
 [gridparams,wpos2D] = make_2Dgrids(xrange,yrange,dx,dy);
 
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 3.b) Pre-allocate ~~~~~~~~~~~~~~~~~~~~~~~~~~
-AS_select= nan(gridparams.Ngp_y,gridparams.Ngp_x,Ntsteps);%swap x and y inpuput arguments since 
-% reshape() will be used to fill in the values
-AS_Hyperbolas = AS_select;
-ASdilate = AS_select;
-
-
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2.c) Compute AS ~~~~~~~~~~~~~~~~~~~~~~~~~~
-for t=1:Ntsteps % for each time step compute LS
-    if selected_indx(t)
-        rp=hyph_pos(:,:,t);
-        ip1=1;ip2=2;
-        % for each hypothetical grid position wpos(wpi) this calculates
-        % time between position and hydrophone position - dt1 (between
-        % hyph 1 and position wpos(wpi)), dt2 (between hyph2 and position wpos(wpi)), then
-        % tdoa_model is tdoa between the hyph1 and hyph2 if the source
-        % is at wpos(wpi).
-        dt1= 1/c.*sqrt((rp(ip1,1)-wpos2D(:,1)).^2 +(rp(ip1,2)-wpos2D(:,2)).^2);
-        dt2= 1/c.*sqrt((rp(ip2,1)-wpos2D(:,1)).^2 +(rp(ip2,2)-wpos2D(:,2)).^2);
-        tdoa_model=dt1-dt2;
-        tdoa_model=reshape(tdoa_model,[gridparams.Ngp_y,gridparams.Ngp_x]);
-
-        tdoa_diff=(tdoa_model-tdoa_measured_select(:,t)).^2;
-        AS_select(:,:,t)= exp(-1/(2*sig^2).*tdoa_diff);
-        AS_Hyperbolas(:,:,t)= exp(-1/(2*sig_hyperbolas^2).*tdoa_diff);
-
-        %///////////////////DILATE SURFACES////////////////////////////////
-        % Apply Surface dilation filter (imdilate) to compensate for whale movement:
-        AStotal_temp=AS_select(:,:,t);
-        dt90 = timestep*abs(t-tstep90); %Elapsed time from when animals are at 90deg (0 tdoa) [s]
-        if dt90==0
-
-            ASdilate(:,:,t)=AStotal_temp; %do not dilate if animal is at the beam (the tdoas should be correct)
-
-        else
-            mdwh=dt90*mean_horiz_swimspeed; %maximum distance whale could have travelled horizontally [m]
-
-            %gridpoints for filter in x (and also y since the same assumptions re swim speed and grid space)
-            xgridf=0:dx:(mdwh+dx);
-            filt_x_grid = [-fliplr(xgridf(2:end)),xgridf];
-            ygridf=0:dy:(mdwh+dy);
-            filt_y_grid = [-fliplr(ygridf(2:end)),ygridf];
-            [Fx,Fy] = meshgrid(filt_x_grid,filt_y_grid);
-            De = sqrt(Fx.^2+Fy.^2)/mdwh; %normalize to max distance that the whale could have swam (feasible distance will be 1 or lower)
-
-            ASdilate(:,:,t)=imdilate(AStotal_temp,(De<=1));
-        end
-        %//////////////////////////////////////////////////////////////////
-
-    end
-end
-clear AStotal_temp
-
-% Get total AS without dilation:
-AStotal=prod(AS_select,3,'omitnan');
-
-% Get total AS with dilation:
-ASdilatetotal=prod(ASdilate,3,'omitnan');
-
-% Get intersecting hyperbolas:
-AStotal_hyperbolas = sum(AS_Hyperbolas,3,'omitnan');
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 3.b) Compute AS ~~~~~~~~~~~~~~~~~~~~~~~~~~
+[AStotal,ASdilatetotal,AStotal_hyperbolas] = obtain_ambiguitysurface(hyph_pos,selected_indx, ...
+    AS_params,BA_params,gridparams,wpos2D,tdoa_measured_select,sig,tstep90);
 
 %add total grid- wpos2D to the gridparams:
 gridparams.wpos = wpos2D;
